596d3dd9b5b39e56411e9cd1570cd8f8a5be55a2
Chok-Ketzamtzib <wakeforest.ww@gmail.com>
2019-07-06 16:55:37 -0400

    Updates

---
+++ precache-manifest.e6c59dbc10a7a5573146ee3dcfe82150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b24a930999bcebca7676087e1a1173",
+    "url": "/williams-portfolio/index.html"
+  },
+  {
+    "revision": "a7466629922d51acffe7",
+    "url": "/williams-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/williams-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a7466629922d51acffe7",
+    "url": "/williams-portfolio/static/js/main.cb781e99.chunk.js"
+  },
+  {
+    "revision": "2a485da77295a546b6b6",
+    "url": "/williams-portfolio/static/js/runtime~main.ac8f74f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/williams-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
